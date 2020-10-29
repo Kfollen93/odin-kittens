7a91627889732c5a5efc35941d62215734cf5c7e
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-10-29 14:07:54 -0700

    Create kitten controller methods and build forms

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,53 @@
 class KittensController < ApplicationController
+  def new
+    @kitten = Kitten.new
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      flash[:notice] = "A cute kitten has been created!"
+      redirect_to kitten_path(@kitten)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      flash.notice = "'#{@kitten.name}' has been updated!'"
+      redirect_to kitten_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    flash.notice = "'#{@kitten.name}' has been let go!"
+    redirect_to kittens_path
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+
 end
